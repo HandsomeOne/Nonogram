dfd96738a7a440a34acdfa984af1a1db19bc2264
handsomeOne <i@handsomeone.com>
2017-02-09 13:15:58 +0800

    webpack 2 and yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
+const webpack = require('webpack')
+const { resolve } = require('path')
+
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   target: 'web',
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
+      exclude: /node_modules/,
       loader: 'babel-loader',
-      query: {
-        presets: ['latest'],
+      options: {
+        presets: [
+          ['es2015', {
+            module: false,
+          }],
+        ],
       },
     }],
   },
-  entry: './src/index.js',
+  entry: isProd ? './src/index.js' : [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js',
+  ],
   output: {
-    path: './test',
-    filename: 'nonogram.js',
+    path: resolve(__dirname, isProd ? 'docs' : 'test'),
+    filename: isProd ? 'nonogram.min.js' : 'nonogram.js',
     library: 'nonogram',
     libraryTarget: 'umd',
   },
-  resolve: {
-    extensions: ['', '.js'],
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: resolve(__dirname, 'test'),
+    inline: true,
+    hot: true,
+    port: 8080,
   },
 }
